5ce257bc81277dc37475c628f139bb0e0a2dceae
Bryan Schauerte <bryan.schauerte@gmail.com>
2017-02-14 01:34:25 -0700

    adding readme, needs cleanup

---
+++ README.md
@@ -0,0 +1,31 @@
+
+
+## Cached Node Server With Jade Template Engine
+
+###Express
+
+- helmet handles headers for security
+- bodyParser to expose and populate req.body
+
+#### Loredis
+- Can be used to set an expiration time on static resources
+
+#### express-redis-cache
+- Used to cache entire routes
+- Can have an expiration data
+
+#### Jade
+- A template engine
+- Can be used to make template for React, Angular or just plain html/jquery
+- Can pull in variables and render specific views
+
+#### Browserify
+- Allows the use of ES6 and modules
+
+#### Security
+- Cors
+- Helment
+- Errors stored and processes exited
+
+#### Compression
+- Compresses and produces faster responses
--- server.js
+++ server.js
@@ -2,7 +2,6 @@ import express from 'express';
 import browserify from 'browserify-middleware';
 const fs = require('fs');
 const path = require('path');
-const PORT = process.env.PORT || 8080;
 const bodyParser =require('body-parser');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
@@ -10,18 +9,17 @@ const expressJWT = require('express-jwt');
 const compression = require('compression');
 const helmet = require('helmet');
 const app = express();
+// template engine
+var jade = require('jade');
+const PORT = process.env.PORT || 8080;
+const sixHours = 43200;
 
 // loredis to have easier access to expiration, just for cacheParamToy route
 var Redis = require('ioredis');
 var redis = new Redis();
 
 // caching routes -> anything complied on the server (*cough *cought react)
-var cache = require('express-redis-cache')({ expire: 60 });
-
-// Jade for templeting but would be amazing for iso react
-
-var jade = require('jade');
-
+var cache = require('express-redis-cache')({ expire: sixHours });
 // to allow es6 and modules
 browserify.settings.mode = 'production';
 browserify.settings({transform: ['babelify']});
@@ -34,33 +32,22 @@ app.use(helmet.ieNoOpen());
 app.use(helmet.noSniff());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: 'application/json' }))
-
-
-//jade view engine
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jade')
-
-
 app.use('/assets', express.static('assets'));
 
 //testing speed of param requests
-// Also how one would do an api call that may need to be chached
 //ex. postman get request http://localhost:3000/cacheParamToy?someKey=22
-app.get('/cacheParamToy', cacheIt, nexTick);
-
+app.get('/cacheParamToy', cacheIt);
 // Whole route is cache, condition can be swapped with a cookie or session ect.
-app.get('/jadeRoute', useCache, renderJade)
-
-
-// route handlers
-function useCache(req, res,next){
-  !req.query.ignoreCache? cache.route({name:'jadeRoute'}): next();
-}
+app.get('/jadeRoute', useCache, renderJade);
 
 function renderJade(req, res){
   res.render('viewTemp', { title: 'Hey', message: 'Hello there!' })
 }
-
+function useCache(req, res,next){
+  !req.query.ignoreCache? cache.route({name:'jadeRoute'}): next();
+}
 function shouldCompress (req, res) {
   if (req.headers['x-no-compression']) {
     return false
@@ -72,27 +59,16 @@ function cacheIt(req, res, next) {
     const someKey = req.query.someKey;
     redis.get('someKey', function (err, data) {
         if (err) throw err;
-        if(data == null){
-          redis.set('someKey', req.query.someKey);
-          return res.send('i was not cached')
-        }
         if (data != null) {
             res.send(`someKey cached value, ${data}`);
         } else {
-            next();
-        }
+          redis.set('someKey', req.query.someKey);
+          return res.send('i was not cached')
+              }
     });
 }
 
-function nexTick(req, res, next) {
-    return res.send('Im the next tick')
-};
-
-//
 ///  [error handling]  ///
-//
-
-
 process.on('uncaughtException', function (er) {
   console.error(er.stack)
   process.exit(1)
